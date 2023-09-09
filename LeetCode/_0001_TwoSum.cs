fadc72b0f4fa4cc73748211f35d20a302773ceb1
imckl <imckl@outlook.com>
2023-09-09 14:30:19 +0800

    update: add _0001_TwoSum

---
+++ LeetCode/_0001_TwoSum.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode;
+
+public class _0001_TwoSum
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var numsByNums = new Dictionary<int, int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (numsByNums.TryGetValue(target - nums[i], out var idx))
+            {
+                return new[] { idx, i };
+            }
+
+            numsByNums.TryAdd(nums[i], i);
+        }
+
+        return Array.Empty<int>();
+    }
+}
