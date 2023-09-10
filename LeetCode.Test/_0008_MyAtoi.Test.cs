260b77519ec93a52ac6602fa4111550a9a3087cf
imckl <imckl@outlook.com>
2023-09-11 02:33:09 +0800

    update: add _0008_MyAtoi

---
+++ LeetCode.Test/_0008_MyAtoi.Test.cs
@@ -0,0 +1,157 @@
+﻿namespace LeetCode.Test;
+
+public class _0008_MyAtoi_Test
+{
+    [Fact]
+    public void NormalTest1()
+    {
+        var solution = new _0008_MyAtoi();
+        var result   = solution.MyAtoi("42");
+
+        Assert.Equal(42, result);
+    }
+
+    [Fact]
+    public void NormalTest2()
+    {
+        var solution = new _0008_MyAtoi();
+        var result   = solution.MyAtoi("   -42");
+
+        Assert.Equal(-42, result);
+    }
+
+    [Fact]
+    public void NormalTest3()
+    {
+        var solution = new _0008_MyAtoi();
+        var result   = solution.MyAtoi("4193 with words");
+
+        Assert.Equal(4193, result);
+    }
+
+    [Fact]
+    public void NormalTest4()
+    {
+        var solution = new _0008_MyAtoi();
+        var result   = solution.MyAtoi("4193.31 with words");
+
+        Assert.Equal(4193, result);
+    }
+
+    [Fact]
+    public void NormalTest5()
+    {
+        var solution = new _0008_MyAtoi();
+        var result   = solution.MyAtoi("41-93.31 with words");
+
+        Assert.Equal(41, result);
+    }
+
+    [Fact]
+    public void NormalTest6()
+    {
+        var solution = new _0008_MyAtoi();
+        var result   = solution.MyAtoi(" - 4193.31 with words");
+
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void NormalTest7()
+    {
+        var solution = new _0008_MyAtoi();
+        var result   = solution.MyAtoi(" -00004193.31 with words");
+
+        Assert.Equal(-4193, result);
+    }
+
+    [Fact]
+    public void NormalTest8()
+    {
+        var solution = new _0008_MyAtoi();
+        var result   = solution.MyAtoi(" -0000.31 with words");
+
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void OneTest()
+    {
+        var solution = new _0008_MyAtoi();
+        var result   = solution.MyAtoi("1");
+
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void MinusOneTest()
+    {
+        var solution = new _0008_MyAtoi();
+        var result   = solution.MyAtoi("-1");
+
+        Assert.Equal(-1, result);
+    }
+
+    [Fact]
+    public void ZeroTest()
+    {
+        var solution = new _0008_MyAtoi();
+        var result   = solution.MyAtoi("0");
+
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void MinusZeroTest()
+    {
+        var solution = new _0008_MyAtoi();
+        var result   = solution.MyAtoi("-0");
+
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void MaxIntTest()
+    {
+        var solution = new _0008_MyAtoi();
+        var result   = solution.MyAtoi("2147483647");
+
+        Assert.Equal(2147483647, result);
+    }
+
+    [Fact]
+    public void MinIntTest()
+    {
+        var solution = new _0008_MyAtoi();
+        var result   = solution.MyAtoi("-2147483648");
+
+        Assert.Equal(-2147483648, result);
+    }
+
+    [Fact]
+    public void MaxIntPlusOneTest()
+    {
+        var solution = new _0008_MyAtoi();
+        var result   = solution.MyAtoi("2147483648");
+
+        Assert.Equal(2147483647, result);
+    }
+
+    [Fact]
+    public void MinIntMinusOneTest()
+    {
+        var solution = new _0008_MyAtoi();
+        var result   = solution.MyAtoi("-2147483649");
+
+        Assert.Equal(-2147483648, result);
+    }
+
+    [Fact]
+    public void EdgeCaseNegativeTest()
+    {
+        var solution = new _0008_MyAtoi();
+        var result   = solution.MyAtoi("-91283472332");
+
+        Assert.Equal(-2147483648, result);
+    }
+}
---
+++ LeetCode/_0008_MyAtoi.cs
@@ -0,0 +1,261 @@
+﻿namespace LeetCode;
+
+public class _0008_MyAtoi
+{
+    // 题设主要考察有限状态机, 没有限定只能使用int
+    public int MyAtoi(string s)
+    {
+        var automaton = new Automaton();
+
+        foreach (var c in s)
+        {
+            automaton.Run(c);
+
+            if (automaton.Break)
+            {
+                break;
+            }
+        }
+
+        return (int)automaton.Result;
+    }
+
+    class Automaton
+    {
+        enum State
+        {
+            Start,
+            Signed,
+            InNumber,
+            End
+        }
+
+        public bool Break;
+        public long Result;
+
+        const long MaxInt = int.MaxValue;
+        const long MinInt = int.MinValue;
+
+        int   _sign  = 1;
+        State _state = State.Start;
+
+        /**
+         *              ' '     +/-     number      other
+         * Start        Start   Signed  InNumber    End
+         * Signed       End     End     InNumber    End
+         * InNumber     End     End     InNumber    End
+         * End          End     End     End         End
+         */
+        readonly Dictionary<State, State[]> _stateTable = new()
+        {
+            { State.Start, new[] { State.Start, State.Signed, State.InNumber, State.End } },
+            { State.Signed, new[] { State.End, State.End, State.InNumber, State.End } },
+            { State.InNumber, new[] { State.End, State.End, State.InNumber, State.End } },
+            { State.End, new[] { State.End, State.End, State.End, State.End } }
+        };
+
+        public void Run(char c)
+        {
+            _state = _stateTable[_state][GetStateCol(c)];
+
+            switch (_state)
+            {
+                case State.InNumber:
+                {
+                    Result = Result * 10 + _sign * (c - '0');
+
+                    if (Result > MaxInt)
+                    {
+                        Result = MaxInt;
+                        Break  = true;
+                    }
+
+                    if (Result < MinInt)
+                    {
+                        Result = MinInt;
+                        Break  = true;
+                    }
+
+                    break;
+                }
+                case State.Signed:
+                {
+                    if (c == '-')
+                    {
+                        _sign = -1;
+                    }
+
+                    break;
+                }
+                case State.Start: break;
+                case State.End:
+                {
+                    Break = true;
+                    break;
+                }
+                default: throw new ArgumentException();
+            }
+        }
+
+        int GetStateCol(char c) => c switch
+        {
+            ' '                                                                => 0,
+            '+' or '-'                                                         => 1,
+            '0' or '1' or '2' or '3' or '4' or '5' or '6' or '7' or '8' or '9' => 2,
+            _                                                                  => 3
+        };
+    }
+}
+
+class _0008_MyAtoi_2
+{
+    enum State
+    {
+        ClearPrefixWhiteSpace,
+        AppendDigit,
+        FinishAssembly
+    }
+
+    enum Action
+    {
+        RemoveWhiteSpace,
+        AppendSign,
+        AppendDigit,
+    }
+
+    readonly List<char>      _chars      = new();
+    readonly HashSet<Action> _actionsSet = new();
+
+    readonly char[] _signs  = { '+', '-' };
+    readonly char[] _digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+    readonly Dictionary<char, int> _numsByChars = new()
+    {
+        { '0', 0 },
+        { '1', 1 },
+        { '2', 2 },
+        { '3', 3 },
+        { '4', 4 },
+        { '5', 5 },
+        { '6', 6 },
+        { '7', 7 },
+        { '8', 8 },
+        { '9', 9 }
+    };
+
+    public int MyAtoi(string s)
+    {
+        var state = State.ClearPrefixWhiteSpace;
+
+        foreach (var c in s)
+        {
+            state = Run(c, state);
+
+            if (state == State.FinishAssembly)
+            {
+                break;
+            }
+        }
+
+        if (!_actionsSet.Contains(Action.AppendDigit))
+        {
+            return 0;
+        }
+
+        return ParseListToInt(_chars);
+    }
+
+    State Run(char c, State state)
+    {
+        switch (state)
+        {
+            case State.ClearPrefixWhiteSpace:
+                if (c == ' ')
+                {
+                    _actionsSet.Add(Action.RemoveWhiteSpace);
+                    return State.ClearPrefixWhiteSpace;
+                }
+
+                if (_signs.Contains(c))
+                {
+                    _chars.Add(c);
+                    _actionsSet.Add(Action.AppendSign);
+                    return State.AppendDigit;
+                }
+
+                if (_digits.Contains(c))
+                {
+                    _chars.Add(c);
+                    _actionsSet.Add(Action.AppendDigit);
+                    return State.AppendDigit;
+                }
+
+                return State.FinishAssembly;
+            case State.AppendDigit:
+                if (_digits.Contains(c))
+                {
+                    _chars.Add(c);
+                    _actionsSet.Add(Action.AppendDigit);
+                    return State.AppendDigit;
+                }
+
+                return State.FinishAssembly;
+            default:
+                throw new ArgumentException();
+        }
+    }
+
+    // 返回 int.MinValue, int.MaxValue 的判断条件的证明参考 _0007_Reverse
+    int ParseListToInt(List<char> chars)
+    {
+        var isNegative = false;
+
+        switch (chars[0])
+        {
+            case '-':
+                isNegative = true;
+                _chars.RemoveAt(0);
+                break;
+            case '+':
+                _chars.RemoveAt(0);
+                break;
+        }
+
+        var len = chars.Count;
+
+        while (len > 0 && chars[0] == '0')
+        {
+            chars.RemoveAt(0);
+            len--;
+        }
+
+        if (len == 0)
+        {
+            return 0;
+        }
+
+        var flag   = isNegative ? -1 : 1;
+        var result = flag * _numsByChars[chars[0]];
+
+        chars.RemoveAt(0);
+
+        while (chars.Count > 0)
+        {
+            if (result < int.MinValue / 10 || (result == int.MinValue / 10 && _numsByChars[chars[0]] > 8))
+            {
+                return int.MinValue;
+            }
+
+            if (result > int.MaxValue / 10 || (result == int.MaxValue / 10 && _numsByChars[chars[0]] > 7))
+            {
+                return int.MaxValue;
+            }
+
+            result = result * 10 + flag * _numsByChars[chars[0]];
+
+            chars.RemoveAt(0);
+        }
+
+        return result;
+    }
+}
