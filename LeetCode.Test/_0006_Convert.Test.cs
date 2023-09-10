0cb36291f7cca3f0b295c83d1d45addbdd0a7648
imckl <imckl@outlook.com>
2023-09-10 14:11:13 +0800

    update: add _0006_Convert

---
+++ LeetCode.Test/_0006_Convert.Test.cs
@@ -0,0 +1,61 @@
+﻿namespace LeetCode.Test;
+
+public class _0006_Convert_Test
+{
+    [Fact]
+    public void ShortTest()
+    {
+        var s = "A";
+
+        var solution = new _0006_Convert();
+        var result   = solution.Convert(s, 1);
+
+        Assert.Equal("A", result);
+    }
+
+    [Fact]
+    public void ShortTest2()
+    {
+        var s = "AB";
+
+        var solution = new _0006_Convert();
+        var result   = solution.Convert(s, 2);
+
+        Assert.Equal("AB", result);
+    }
+
+    [Fact]
+    public void NormalTest()
+    {
+        // 012345678901234567890123456789012345678901234567890123456
+
+        var s = "abcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefg";
+
+        var solution = new _0006_Convert();
+        var result   = solution.Convert(s, 5);
+
+        Assert.Equal("aigecaigbhjfhdfbdjbhjfcgaeicgaeicgaedfbdjbhjfhdfbdecaigec", result);
+    }
+
+    [Fact]
+    public void NormalTest2()
+    {
+        var s = "PAYPALISHIRING";
+
+        var solution = new _0006_Convert();
+        var result   = solution.Convert(s, 3);
+
+        Assert.Equal("PAHNAPLSIIGYIR", result);
+    }
+
+    [Fact]
+    public void NormalTest3()
+    {
+        var s = "PAYPALISHIRING";
+
+        var solution = new _0006_Convert();
+        var result   = solution.Convert(s, 4);
+
+        Assert.Equal("PINALSIGYAHRPI", result);
+    }
+}
---
+++ LeetCode/_0006_Convert.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+
+namespace LeetCode;
+
+public class _0006_Convert
+{
+    public string Convert(string s, int numRows)
+    {
+        if (numRows == 1)
+        {
+            return s;
+        }
+
+        var resultBuilder = new StringBuilder();
+        // var result = "";
+
+        var len = s.Length;
+
+        {
+            var step = (numRows - 1) * 2;
+            var i    = 0;
+            while (i < len)
+            {
+                // result += s[i];
+                resultBuilder.Append(s[i]);
+
+                if (i + step > len)
+                {
+                    break;
+                }
+
+                i += step;
+            }
+        }
+
+        for (var n = 1; n < numRows - 1; n++)
+        {
+            var evenStep = (numRows - 1 - n) * 2;
+            var oddStep  = n                 * 2;
+
+            var i     = n;
+            var count = 0;
+            while (i < len)
+            {
+                // result += s[i];
+                resultBuilder.Append(s[i]);
+
+                var step = count % 2 == 0 ? evenStep : oddStep;
+
+                if (i + step > len)
+                {
+                    break;
+                }
+
+                i += step;
+                count++;
+            }
+        }
+
+        {
+            var step = (numRows - 1) * 2;
+            var i    = numRows - 1;
+            while (i < len)
+            {
+                // result += s[i];
+                resultBuilder.Append(s[i]);
+
+                if (i + step > len)
+                {
+                    break;
+                }
+
+                i += step;
+            }
+        }
+
+        // return result;
+        return resultBuilder.ToString();
+    }
+}
