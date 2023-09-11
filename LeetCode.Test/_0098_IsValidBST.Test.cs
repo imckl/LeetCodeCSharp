ceff40b2e5f64bfb7c80dfe86ca0767b38010387
imckl <imckl@outlook.com>
2023-09-12 00:08:09 +0800

    update: add _NotSolved_0098_IsValidBST

---
+++ LeetCode.Test/_0098_IsValidBST.Test.cs
@@ -0,0 +1,116 @@
+﻿using LeetCode.Structs;
+
+namespace LeetCode.Test;
+
+public class _0098_IsValidBST_Test
+{
+    [Fact]
+    public void NormalTest1()
+    {
+        var root = TreeNode.FromArray(new int?[] { 2, 1, 3 })!;
+
+        var solution = new _NotSolved_0098_IsValidBST();
+        var actual   = solution.IsValidBST(root);
+
+        Assert.True(actual);
+    }
+
+    [Fact]
+    public void NormalTest2()
+    {
+        var root = TreeNode.FromArray(new int?[] { 5, 1, 4, null, null, 3, 6 })!;
+
+        var solution = new _NotSolved_0098_IsValidBST();
+        var actual   = solution.IsValidBST(root);
+
+        Assert.False(actual);
+    }
+
+    [Fact]
+    public void NormalTest3()
+    {
+        var root = TreeNode.FromArray(new int?[] { 5, 2, 8, null, 1, 7, 9 })!;
+
+        var solution = new _NotSolved_0098_IsValidBST();
+        var actual   = solution.IsValidBST(root);
+
+        Assert.False(actual);
+    }
+
+    [Fact]
+    public void NormalTest4()
+    {
+        var root = TreeNode.FromArray(new int?[] { 5, 2, 8, null, 3, 7, 9 })!;
+
+        var solution = new _NotSolved_0098_IsValidBST();
+        var actual   = solution.IsValidBST(root);
+
+        Assert.True(actual);
+    }
+
+    [Fact]
+    public void NormalTest5()
+    {
+        var root = TreeNode.FromArray(new int?[] { 5, 2, 8, null, 3, 7, 9 })!;
+
+        var solution = new _NotSolved_0098_IsValidBST();
+        var actual   = solution.IsValidBST(root);
+
+        Assert.True(actual);
+    }
+
+    [Fact]
+    public void NormalTest6()
+    {
+        var root = TreeNode.FromArray(new int?[] { 1, 1, 1 })!;
+
+        var solution = new _NotSolved_0098_IsValidBST();
+        var actual   = solution.IsValidBST(root);
+
+        Assert.False(actual);
+    }
+
+    [Fact]
+    public void NormalTest7()
+    {
+        var root = TreeNode.FromArray(new int?[] { 1, 1 })!;
+
+        var solution = new _NotSolved_0098_IsValidBST();
+        var actual   = solution.IsValidBST(root);
+
+        Assert.False(actual);
+    }
+
+    [Fact]
+    public void NormalTest8()
+    {
+        var root = TreeNode.FromArray(new int?[] { 1, null, 1 })!;
+
+        var solution = new _NotSolved_0098_IsValidBST();
+        var actual   = solution.IsValidBST(root);
+
+        Assert.False(actual);
+    }
+
+    [Fact]
+    public void NormalTest9()
+    {
+        var root = TreeNode.FromArray(new int?[] { 1 })!;
+
+        var solution = new _NotSolved_0098_IsValidBST();
+        var actual   = solution.IsValidBST(root);
+
+        Assert.True(actual);
+    }
+
+    [Fact]
+    public void MinMaxIntTest()
+    {
+        var root = TreeNode.FromArray(new int?[] { 1, -2147483648, 2147483647 })!;
+
+        var solution = new _NotSolved_0098_IsValidBST();
+        var actual   = solution.IsValidBST(root);
+
+        Assert.True(actual);
+    }
+}
---
+++ LeetCode/_NotSolved_0098_IsValidBST.cs
@@ -0,0 +1,40 @@
+﻿using LeetCode.Structs;
+
+namespace LeetCode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// 搞清楚二叉树后，再尝试中序遍历，前序遍历，后序遍历，及以上递归版本
+public class _NotSolved_0098_IsValidBST
+{
+    public bool IsValidBST(TreeNode root)
+    {
+        return IsValidBST(root, long.MinValue, long.MaxValue);
+    }
+
+    bool IsValidBST(TreeNode? node, long lower, long upper)
+    {
+        if (node is null)
+        {
+            return true;
+        }
+
+        if (node.val <= lower || node.val >= upper)
+        {
+            return false;
+        }
+
+        return IsValidBST(node.left, lower, node.val) && IsValidBST(node.right, node.val, upper);
+    }
+}
