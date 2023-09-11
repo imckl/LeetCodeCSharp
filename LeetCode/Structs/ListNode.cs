9782ea5e0b4925c2e7ccd289c25a2ade55f77726
imckl <imckl@outlook.com>
2023-09-11 22:23:00 +0800

    update: ListNode  - add FromArray  - add ToArray

--- LeetCode/Structs/ListNode.cs
+++ LeetCode/Structs/ListNode.cs
@@ -13,12 +13,47 @@
  */
 public class ListNode
 {
-    public readonly int       val;
-    public          ListNode? next;
+    // ReSharper disable InconsistentNaming
+    public readonly int val;
+
+    public ListNode? next;
+    // ReSharper restore InconsistentNaming
 
     public ListNode(int val = 0, ListNode? next = null)
     {
         this.val  = val;
         this.next = next;
+    }
+
+    public static ListNode? FromArray(int[] nums)
+    {
+        if (nums.Length == 0)
+        {
+            return null;
+        }
+
+        var head = new ListNode(nums[0]);
+        var ln   = head;
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            ln.next = new ListNode(nums[i]);
+            ln      = ln.next;
+        }
+
+        return head;
+    }
+
+    public static int[] ToArray(ListNode? node)
+    {
+        var list = new List<int>();
+
+        while (node is not null)
+        {
+            list.Add(node.val);
+            node = node.next;
+        }
+
+        return list.ToArray();
     }
 }
