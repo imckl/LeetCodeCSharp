9aa18097910e713da539b415dbdc176e57d358a1
imckl <imckl@outlook.com>
2023-09-11 22:24:51 +0800

    update: TestHelper

--- LeetCode.Test/TestHelper.cs
+++ LeetCode.Test/TestHelper.cs
@@ -4,38 +4,6 @@ namespace LeetCode.Test;
 
 public static class TestHelper
 {
-    public static ListNode? ArrayToSinglyLinkedList(int[] nums)
-    {
-        if (nums.Length == 0)
-        {
-            return null;
-        }
-
-        var head = new ListNode(nums[0]);
-        var ln   = head;
-
-        for (var i = 1; i < nums.Length; i++)
-        {
-            ln.next = new ListNode(nums[i]);
-            ln      = ln.next;
-        }
-
-        return head;
-    }
-
-    public static int[] SinglyLinkedListToArray(ListNode? ln)
-    {
-        var list = new List<int>();
-
-        while (ln is not null)
-        {
-            list.Add(ln.val);
-            ln = ln.next;
-        }
-
-        return list.ToArray();
-    }
-
     // public static TreeNode GenerateTree(int?[] nums)
     // {
     //     if (nums == null || nums.Length == 0)
