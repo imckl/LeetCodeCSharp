e274a7efefbccafac0487e2a034e6a7e7739db3c
imckl <imckl@outlook.com>
2023-09-11 21:01:53 +0800

    update: add _0019_RemoveNthFromEnd

---
+++ LeetCode.Test/_0019_RemoveNthFromEnd.Test.cs
@@ -0,0 +1,141 @@
+﻿namespace LeetCode.Test;
+
+public class _0019_RemoveNthFromEnd_Test
+{
+    [Fact]
+    public void GetNodeLength_Len1()
+    {
+        var ln = TestHelper.ArrayToSinglyLinkedList(new[] { 1 })!;
+
+        var solution = new _0019_RemoveNthFromEnd();
+        var actual   = solution.GetNodeLength(ln);
+
+        Assert.Equal(1, actual);
+    }
+
+    [Fact]
+    public void GetNodeLength_Len2()
+    {
+        var ln = TestHelper.ArrayToSinglyLinkedList(new[] { 1, 2 })!;
+
+        var solution = new _0019_RemoveNthFromEnd();
+        var actual   = solution.GetNodeLength(ln);
+
+        Assert.Equal(2, actual);
+    }
+
+    [Fact]
+    public void GetNodeLength_Len3()
+    {
+        var ln = TestHelper.ArrayToSinglyLinkedList(new[] { 1, 2, 3 })!;
+
+        var solution = new _0019_RemoveNthFromEnd();
+        var actual   = solution.GetNodeLength(ln);
+
+        Assert.Equal(3, actual);
+    }
+
+    [Fact]
+    public void RemoveNthNode_TestLengthOne()
+    {
+        var ln = TestHelper.ArrayToSinglyLinkedList(new[] { 1 })!;
+
+        var solution = new _0019_RemoveNthFromEnd();
+        var actual   = solution.RemoveNthNode(ln, 1);
+
+        var expected = TestHelper.ArrayToSinglyLinkedList(Array.Empty<int>());
+
+        AssertHelper.AssertSinglyLinkList(expected, actual);
+    }
+
+    [Fact]
+    public void RemoveNthNode_TestFirstOne()
+    {
+        var ln = TestHelper.ArrayToSinglyLinkedList(new[] { 1, 2, 3, 4, 5 })!;
+
+        var solution = new _0019_RemoveNthFromEnd();
+        var actual   = solution.RemoveNthNode(ln, 1);
+
+        var expected = TestHelper.ArrayToSinglyLinkedList(new[] { 2, 3, 4, 5 });
+
+        AssertHelper.AssertSinglyLinkList(expected, actual);
+    }
+
+    [Fact]
+    public void RemoveNthNode_TestMidOne()
+    {
+        var ln = TestHelper.ArrayToSinglyLinkedList(new[] { 1, 2, 3, 4, 5 })!;
+
+        var solution = new _0019_RemoveNthFromEnd();
+        var actual   = solution.RemoveNthNode(ln, 3);
+
+        var expected = TestHelper.ArrayToSinglyLinkedList(new[] { 1, 2, 4, 5 });
+
+        AssertHelper.AssertSinglyLinkList(expected, actual);
+    }
+
+    [Fact]
+    public void RemoveNthNode_TestLast()
+    {
+        var ln = TestHelper.ArrayToSinglyLinkedList(new[] { 1, 2, 3, 4, 5 })!;
+
+        var solution = new _0019_RemoveNthFromEnd();
+        var actual   = solution.RemoveNthNode(ln, 5);
+
+        var expected = TestHelper.ArrayToSinglyLinkedList(new[] { 1, 2, 3, 4 });
+
+        AssertHelper.AssertSinglyLinkList(expected, actual);
+    }
+
+    [Fact]
+    public void Normal_TestLengthOne()
+    {
+        var link = TestHelper.ArrayToSinglyLinkedList(new[] { 1 })!;
+
+        var solution = new _0019_RemoveNthFromEnd();
+        var actual   = solution.RemoveNthFromEnd(link, 1);
+
+        var expected = TestHelper.ArrayToSinglyLinkedList(Array.Empty<int>());
+
+        AssertHelper.AssertSinglyLinkList(expected, actual);
+    }
+
+    [Fact]
+    public void Normal_TestFirstOne()
+    {
+        var link = TestHelper.ArrayToSinglyLinkedList(new[] { 1, 2, 3, 4, 5 })!;
+
+        var solution = new _0019_RemoveNthFromEnd();
+        var actual   = solution.RemoveNthFromEnd(link, 5);
+
+        var expected = TestHelper.ArrayToSinglyLinkedList(new[] { 2, 3, 4, 5 });
+
+        AssertHelper.AssertSinglyLinkList(expected, actual);
+    }
+
+    [Fact]
+    public void Normal_TestMidOne()
+    {
+        var link = TestHelper.ArrayToSinglyLinkedList(new[] { 1, 2, 3, 4, 5 })!;
+
+        var solution = new _0019_RemoveNthFromEnd();
+        var actual   = solution.RemoveNthFromEnd(link, 3);
+
+        var expected = TestHelper.ArrayToSinglyLinkedList(new[] { 1, 2, 4, 5 });
+
+        AssertHelper.AssertSinglyLinkList(expected, actual);
+    }
+
+    [Fact]
+    public void Normal_TestLastOne()
+    {
+        var link = TestHelper.ArrayToSinglyLinkedList(new[] { 1, 2, 3, 4, 5 })!;
+
+        var solution = new _0019_RemoveNthFromEnd();
+        var actual   = solution.RemoveNthFromEnd(link, 1);
+
+        var expected = TestHelper.ArrayToSinglyLinkedList(new[] { 1, 2, 3, 4 });
+
+        AssertHelper.AssertSinglyLinkList(expected, actual);
+    }
+}
---
+++ LeetCode/_0019_RemoveNthFromEnd.cs
@@ -0,0 +1,106 @@
+﻿using LeetCode.Structs;
+
+namespace LeetCode;
+
+public class _0019_RemoveNthFromEnd
+{
+    // 快慢指针, 注意慢指针选取的起点是dummy - 永远比快指针慢?
+    public ListNode? RemoveNthFromEnd(ListNode head, int n)
+    {
+        var dummy = new ListNode(0, head);
+        var fast  = head;
+        var slow  = dummy;
+
+        for (var i = 0; i < n; i++)
+        {
+            fast = fast!.next;
+        }
+
+        while (fast is not null)
+        {
+            fast = fast.next;
+            slow = slow!.next;
+        }
+
+        slow!.next = slow.next!.next;
+
+        return dummy.next;
+    }
+
+    public ListNode? RemoveNthFromEnd_3(ListNode head, int n)
+    {
+        var listNodesByIndexes = new Dictionary<int, ListNode>();
+
+        var dummy = new ListNode(0, head);
+        var node  = dummy;
+        var i     = 0;
+
+        while (node is not null)
+        {
+            listNodesByIndexes.Add(i, node);
+
+            i++;
+            node = node.next;
+        }
+
+        var len      = listNodesByIndexes.Count;
+        var removeAt = len - n;
+
+        listNodesByIndexes[removeAt - 1].next = listNodesByIndexes[removeAt].next;
+
+        return dummy.next;
+    }
+
+    public int GetNodeLength(ListNode head)
+    {
+        var len  = 0;
+        var node = head;
+
+        while (node is not null)
+        {
+            node = node.next;
+            len++;
+        }
+
+        return len;
+    }
+
+    public ListNode? RemoveNthNode(ListNode head, int n)
+    {
+        if (head.next is null)
+        {
+            return null;
+        }
+
+        var dummy = new ListNode(0, head);
+        var node  = dummy;
+
+        var m = n - 1;
+
+        for (var i = 0; i < m; i++)
+        {
+            node = node!.next;
+        }
+
+        if (node!.next is not null)
+        {
+            node.next = node.next.next;
+        }
+        else
+        {
+            node.next = null;
+        }
+
+        return dummy.next;
+    }
+
+    public ListNode? RemoveNthFromEnd_2(ListNode head, int n)
+    {
+        var len      = GetNodeLength(head);
+        var removeAt = len - n + 1;
+
+        var node = RemoveNthNode(head, removeAt);
+
+        return node;
+    }
+}
