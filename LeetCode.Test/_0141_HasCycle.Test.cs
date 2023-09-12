3a2e928154b16f172503956f48797caf658212de
imckl <imckl@outlook.com>
2023-09-12 13:59:06 +0800

    update: add _0141_HasCycle

---
+++ LeetCode.Test/_0141_HasCycle.Test.cs
@@ -0,0 +1,92 @@
+﻿using LeetCode.Structs;
+
+namespace LeetCode.Test;
+
+public class _0141_HasCycle_Test
+{
+    [Fact]
+    public void CycleItself()
+    {
+        var first = new ListNode(0);
+        first.next = first;
+
+        var solution = new _0141_HasCycle();
+        var actual   = solution.HasCycle(first);
+
+        Assert.True(actual);
+    }
+
+    [Fact]
+    public void CycleToHead()
+    {
+        var first  = new ListNode(0);
+        var second = new ListNode();
+        var third  = new ListNode();
+        first.next  = second;
+        second.next = third;
+        third.next  = first;
+
+        var solution = new _0141_HasCycle();
+        var actual   = solution.HasCycle(first);
+
+        Assert.True(actual);
+    }
+
+    [Fact]
+    public void CycleToMid()
+    {
+        var first  = new ListNode(0);
+        var second = new ListNode();
+        var third  = new ListNode();
+        first.next  = second;
+        second.next = third;
+        third.next  = second;
+
+        var solution = new _0141_HasCycle();
+        var actual   = solution.HasCycle(first);
+
+        Assert.True(actual);
+    }
+
+    [Fact]
+    public void CycleToLast()
+    {
+        var first  = new ListNode(0);
+        var second = new ListNode();
+        var third  = new ListNode();
+        first.next  = second;
+        second.next = third;
+        third.next  = third;
+
+        var solution = new _0141_HasCycle();
+        var actual   = solution.HasCycle(first);
+
+        Assert.True(actual);
+    }
+
+    [Fact]
+    public void NoCycle()
+    {
+        var first  = new ListNode(0);
+        var second = new ListNode();
+        var third  = new ListNode();
+        first.next  = second;
+        second.next = third;
+
+        var solution = new _0141_HasCycle();
+        var actual   = solution.HasCycle(first);
+
+        Assert.False(actual);
+    }
+
+    [Fact]
+    public void ZeroListNode()
+    {
+        ListNode? first = null;
+
+        var solution = new _0141_HasCycle();
+        var actual   = solution.HasCycle(first);
+
+        Assert.False(actual);
+    }
+}
---
+++ LeetCode/_0141_HasCycle.cs
@@ -0,0 +1,75 @@
+﻿using LeetCode.Structs;
+
+namespace LeetCode;
+
+public class _0141_HasCycle
+{
+    public bool HasCycle(ListNode? head)
+    {
+        var dummy = new ListNode(0, head);
+        var fast  = head;
+        var slow  = dummy;
+
+        var i = 0;
+        while (fast is not null)
+        {
+            if (fast == slow)
+            {
+                return true;
+            }
+
+            // 块二慢一
+            if (i % 2 == 0)
+            {
+                slow = slow!.next;
+            }
+
+            fast = fast.next;
+
+            i++;
+        }
+
+        return false;
+    }
+
+    // bad, 修改了链表
+    public bool HasCycle_3(ListNode? head)
+    {
+        var dummy = new ListNode();
+        var node  = head;
+
+        while (node is not null)
+        {
+            if (node.next == dummy)
+            {
+                return true;
+            }
+
+            var cur = node;
+            node     = node.next;
+            cur.next = dummy;
+        }
+
+        return false;
+    }
+
+    public bool HasCycle_2(ListNode? head)
+    {
+        var nodes = new HashSet<ListNode>();
+        var node  = head;
+
+        while (node is not null)
+        {
+            if (nodes.Contains(node))
+            {
+                return true;
+            }
+
+            nodes.Add(node);
+
+            node = node.next;
+        }
+
+        return false;
+    }
+}
