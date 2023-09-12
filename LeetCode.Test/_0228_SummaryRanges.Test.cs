736982eaa6c52651cf30064607597dcf19abd9a1
imckl <imckl@outlook.com>
2023-09-12 18:08:49 +0800

    update: add _0228_SummaryRanges

---
+++ LeetCode.Test/_0228_SummaryRanges.Test.cs
@@ -0,0 +1,74 @@
+﻿namespace LeetCode.Test;
+
+public class _228_SummaryRanges_Test
+{
+    [Fact]
+    public void LengthZero()
+    {
+        var nums     = Array.Empty<int>();
+        var solution = new _0228_SummaryRanges();
+        var actual   = solution.SummaryRanges(nums);
+
+        Assert.Equal(Array.Empty<string>(), actual);
+    }
+
+    [Fact]
+    public void LengthOne()
+    {
+        var nums     = new[] { 0 };
+        var solution = new _0228_SummaryRanges();
+        var actual   = solution.SummaryRanges(nums);
+
+        Assert.Equal(new[] { "0" }, actual);
+    }
+
+    [Fact]
+    public void SingleStart()
+    {
+        var nums     = new[] { -1, 1, 2, 4, 5, 7, 8 };
+        var solution = new _0228_SummaryRanges();
+        var actual   = solution.SummaryRanges(nums);
+
+        Assert.Equal(new[] { "-1", "1->2", "4->5", "7->8" }, actual);
+    }
+
+    [Fact]
+    public void SingleEnd()
+    {
+        var nums     = new[] { 0, 1, 2, 4, 5, 7 };
+        var solution = new _0228_SummaryRanges();
+        var actual   = solution.SummaryRanges(nums);
+
+        Assert.Equal(new[] { "0->2", "4->5", "7" }, actual);
+    }
+
+    [Fact]
+    public void SingleStartAndEnd()
+    {
+        var nums     = new[] { -1, 1, 2, 4, 5, 7 };
+        var solution = new _0228_SummaryRanges();
+        var actual   = solution.SummaryRanges(nums);
+
+        Assert.Equal(new[] { "-1", "1->2", "4->5", "7" }, actual);
+    }
+
+    [Fact]
+    public void EdgeCase()
+    {
+        var nums     = new[] { int.MinValue, 0, int.MaxValue };
+        var solution = new _0228_SummaryRanges();
+        var actual   = solution.SummaryRanges(nums);
+
+        Assert.Equal(new[] { "-2147483648", "0", "2147483647" }, actual);
+    }
+
+    [Fact]
+    public void EdgeCase2()
+    {
+        var nums     = new[] { int.MinValue, -1, 0, 1, int.MaxValue };
+        var solution = new _0228_SummaryRanges();
+        var actual   = solution.SummaryRanges(nums);
+
+        Assert.Equal(new[] { "-2147483648", "-1->1", "2147483647" }, actual);
+    }
+}
---
+++ LeetCode/_0228_SummaryRanges.cs
@@ -0,0 +1,80 @@
+﻿namespace LeetCode;
+
+public class _0228_SummaryRanges
+{
+    public IList<string> SummaryRanges(int[] nums)
+    {
+        if (nums.Length == 0)
+        {
+            return Array.Empty<string>();
+        }
+
+        if (nums.Length == 1)
+        {
+            return new[] { nums[0].ToString()! };
+        }
+
+        var results = new List<string>();
+
+        var len = nums.Length;
+        var i   = 1;
+
+        var pair = new[] { nums[0], nums[0] };
+
+        while (i < len)
+        {
+            if (nums[i] != nums[i - 1] + 1)
+            {
+                pair[1] = nums[i - 1];
+                results.Add(pair[0] == pair[1] ? pair[0].ToString()! : $"{pair[0]}->{pair[1]}");
+                pair = new[] { nums[i], nums[i] };
+            }
+
+            i++;
+        }
+
+        pair[1] = nums[len - 1];
+        results.Add(pair[0] == pair[1] ? pair[0].ToString()! : $"{pair[0]}->{pair[1]}");
+
+        return results;
+    }
+
+    public IList<string> SummaryRanges_02(int[] nums)
+    {
+        if (nums.Length == 0)
+        {
+            return Array.Empty<string>();
+        }
+
+        if (nums.Length == 1)
+        {
+            return new[] { nums[0].ToString()! };
+        }
+
+        var results = new List<string>();
+
+        var len = nums.Length;
+        var i   = 1;
+
+        var pair = new[] { nums[0], nums[0] };
+
+        while (i < len)
+        {
+            if (nums[i] == nums[i - 1] + 1)
+            {
+                pair[1] = nums[i];
+            }
+            else
+            {
+                results.Add(pair[0] == pair[1] ? pair[0].ToString()! : $"{pair[0]}->{pair[1]}");
+                pair = new[] { nums[i], nums[i] };
+            }
+
+            i++;
+        }
+
+        results.Add(pair[0] == pair[1] ? pair[0].ToString()! : $"{pair[0]}->{pair[1]}");
+
+        return results;
+    }
+}
