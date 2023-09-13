1df488acccba665655fc9ec8c5964fb464d3a3e1
imckl <imckl@outlook.com>
2023-09-13 11:33:02 +0800

    update: add _2240_WaysToBuyPensPencils

---
+++ LeetCode.Test/_2240_WaysToBuyPensPencils.Test.cs
@@ -0,0 +1,108 @@
+﻿namespace LeetCode.Test;
+
+public class _2240_WaysToBuyPensPencils_Test
+{
+    [Fact]
+    public void Test_111()
+    {
+        var total = 1;
+        var cost1 = 1;
+        var cost2 = 1;
+
+        var solution = new _2240_WaysToBuyPensPencils();
+        var actual   = solution.WaysToBuyPensPencils(total, cost1, cost2);
+
+        Assert.Equal(3, actual);
+    }
+
+    [Fact]
+    public void Test_NotEnoughTotal()
+    {
+        var total = 1;
+        var cost1 = 10;
+        var cost2 = 10;
+
+        var solution = new _2240_WaysToBuyPensPencils();
+        var actual   = solution.WaysToBuyPensPencils(total, cost1, cost2);
+
+        Assert.Equal(1, actual);
+    }
+
+    [Fact]
+    public void Test_Normal()
+    {
+        var total = 20;
+        var cost1 = 10;
+        var cost2 = 5;
+
+        var solution = new _2240_WaysToBuyPensPencils();
+        var actual   = solution.WaysToBuyPensPencils(total, cost1, cost2);
+
+        Assert.Equal(9, actual);
+    }
+
+    [Fact]
+    public void Test_NotEnoughCost1()
+    {
+        var total = 11;
+        var cost1 = 99;
+        var cost2 = 2;
+
+        var solution = new _2240_WaysToBuyPensPencils();
+        var actual   = solution.WaysToBuyPensPencils(total, cost1, cost2);
+
+        Assert.Equal(6, actual);
+    }
+
+    [Fact]
+    public void Test_Normal3()
+    {
+        var total = 89;
+        var cost1 = 2;
+        var cost2 = 1;
+
+        var solution = new _2240_WaysToBuyPensPencils();
+        var actual   = solution.WaysToBuyPensPencils(total, cost1, cost2);
+
+        Assert.Equal(2070, actual);
+    }
+
+    [Fact]
+    public void Test_Normal4()
+    {
+        var total = 10;
+        var cost1 = 2;
+        var cost2 = 1;
+
+        var solution = new _2240_WaysToBuyPensPencils();
+        var actual   = solution.WaysToBuyPensPencils(total, cost1, cost2);
+
+        Assert.Equal(36, actual);
+    }
+
+    [Fact]
+    public void Test_Normal5()
+    {
+        var total = 11;
+        var cost1 = 2;
+        var cost2 = 1;
+
+        var solution = new _2240_WaysToBuyPensPencils();
+        var actual   = solution.WaysToBuyPensPencils(total, cost1, cost2);
+
+        Assert.Equal(42, actual);
+    }
+
+    [Fact]
+    public void Test_BiggerThanMaxIntResult()
+    {
+        var total = 1000000;
+        var cost1 = 1;
+        var cost2 = 1;
+
+        var solution = new _2240_WaysToBuyPensPencils();
+        var actual   = solution.WaysToBuyPensPencils(total, cost1, cost2);
+
+        Assert.Equal(500001500001, actual);
+    }
+}
---
+++ LeetCode/_2240_WaysToBuyPensPencils.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode;
+
+public class _2240_WaysToBuyPensPencils
+{
+    // 注意max1上下界都为闭区间（可以买[0, max1]个）
+    public long WaysToBuyPensPencils(int total, int cost1, int cost2)
+    {
+        long ans = 0;
+
+        var max1 = total / cost1;
+
+        for (var i = 0; i <= max1; i++)
+        {
+            var left = total - cost1 * i;
+            var max2 = left / cost2;
+
+            ans += max2 + 1;
+        }
+
+        return ans;
+    }
+}
