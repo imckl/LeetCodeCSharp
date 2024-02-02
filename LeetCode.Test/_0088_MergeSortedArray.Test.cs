542750bd6441c28f907fec38510d2e81094ea67c
imckl <imckl@outlook.com>
2024-02-02 20:44:35 +0800

    update: _0088_MergeSortedArray

---
+++ LeetCode.Test/_0088_MergeSortedArray.Test.cs
@@ -0,0 +1,64 @@
+﻿namespace LeetCode.Test;
+
+public class _0088_MergeSortedArray_Test
+{
+    [Fact]
+    public void Test1()
+    {
+        var nums1 = new[] { 1, 2, 3, 0, 0, 0 };
+        var m     = 3;
+
+        var nums2 = new[] { 2, 5, 6 };
+        var n     = 3;
+
+        var solution = new _0088_MergeSortedArray();
+        solution.Merge(nums1, m, nums2, n);
+
+        Assert.Equal(new[] { 1, 2, 2, 3, 5, 6 }, nums1);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var nums1 = new[] { 1 };
+        var m     = 1;
+
+        var nums2 = Array.Empty<int>();
+        var n     = 0;
+
+        var solution = new _0088_MergeSortedArray();
+        solution.Merge(nums1, m, nums2, n);
+
+        Assert.Equal(new[] { 1 }, nums1);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var nums1 = new int[1];
+        var m     = 0;
+
+        var nums2 = new[] { 1 };
+        var n     = 1;
+
+        var solution = new _0088_MergeSortedArray();
+        solution.Merge(nums1, m, nums2, n);
+
+        Assert.Equal(new[] { 1 }, nums1);
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        var nums1 = new[] { -100, -99, -99, 0, 1, 2, 0, 0, 0 };
+        var m     = 6;
+
+        var nums2 = new[] { -101, 5, 6 };
+        var n     = 3;
+
+        var solution = new _0088_MergeSortedArray();
+        solution.Merge(nums1, m, nums2, n);
+
+        Assert.Equal(new[] { -101, -100, -99, -99, 0, 1, 2, 5, 6 }, nums1);
+    }
+}
---
+++ LeetCode/_0088_MergeSortedArray.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCode;
+
+public class _0088_MergeSortedArray
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        var p1 = 0;
+        var p2 = 0;
+
+        var i = 0;
+
+        var nums3 = new int[m + n];
+
+        while (i < m + n && p1 < m && p2 < n)
+        {
+            if (nums1[p1] < nums2[p2])
+            {
+                nums3[i] = nums1[p1];
+                p1++;
+            }
+            else
+            {
+                nums3[i] = nums2[p2];
+                p2++;
+            }
+
+            i++;
+        }
+
+        while (p1 < m)
+        {
+            nums3[i] = nums1[p1];
+            p1++;
+            i++;
+        }
+
+        while (p2 < n)
+        {
+            nums3[i] = nums2[p2];
+            p2++;
+            i++;
+        }
+
+        for (var j = 0; j < nums3.Length; j++)
+        {
+            nums1[j] = nums3[j];
+        }
+    }
+}
