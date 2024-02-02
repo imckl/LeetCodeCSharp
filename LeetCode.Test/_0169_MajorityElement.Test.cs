2860a044699590adbfa78878a3493e1415df32b1
imckl <imckl@outlook.com>
2024-02-03 01:10:23 +0800

    update: _0169_MajorityElement

---
+++ LeetCode.Test/_0169_MajorityElement.Test.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode.Test;
+
+public class _0169_MajorityElement_Test
+{
+    [Fact]
+    public void Test1()
+    {
+        var nums = new[] { 3, 2, 3 };
+
+        var solution = new _0169_MajorityElement();
+        var answer   = solution.MajorityElement(nums);
+
+        Assert.Equal(answer, 3);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var nums = new[] { 3 };
+
+        var solution = new _0169_MajorityElement();
+        var answer   = solution.MajorityElement(nums);
+
+        Assert.Equal(answer, 3);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var nums = new[]
+        {
+            3, 5463, 6534, 63, 6534, 6534, 6, 346, 6534, 6, 5346, 3456, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 453, 6, 365,
+            436534, 634, 5634, 5634, 8, 8, 8, 8, 8, 8, 8, 8, 8, 432
+        };
+
+        var solution = new _0169_MajorityElement();
+        var answer   = solution.MajorityElement(nums);
+
+        Assert.Equal(answer, 8);
+    }
+}
---
+++ LeetCode/_0169_MajorityElement.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode;
+
+public class _0169_MajorityElement
+{
+    public int MajorityElement(int[] nums)
+    {
+        var candidate = nums[0];
+        var count     = 1;
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] == candidate)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+
+            if (count == 0)
+            {
+                candidate = nums[i];
+                count     = 1;
+            }
+        }
+
+        return candidate;
+    }
+}
