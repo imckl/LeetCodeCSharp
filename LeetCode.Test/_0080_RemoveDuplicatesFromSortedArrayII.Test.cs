3e112570d0c9291417cb6d4a36b9d656a5b2d9d8
imckl <imckl@outlook.com>
2024-02-03 00:30:14 +0800

    update: _0080_RemoveDuplicatesFromSortedArrayII

---
+++ LeetCode.Test/_0080_RemoveDuplicatesFromSortedArrayII.Test.cs
@@ -0,0 +1,157 @@
+﻿namespace LeetCode.Test;
+
+public class _0080_RemoveDuplicatesFromSortedArrayII_Test
+{
+    [Fact]
+    public void Test1()
+    {
+        var nums = new[] { 1, 1, 1, 2, 2, 3 };
+
+        var solution = new _0080_RemoveDuplicatesFromSortedArrayII();
+        var length   = solution.RemoveDuplicates(nums);
+
+        var results = new int[length];
+        for (var i = 0; i < results.Length; i++)
+        {
+            results[i] = nums[i];
+        }
+
+        Assert.Equal(results, new[] { 1, 1, 2, 2, 3 });
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var nums = new[] { 0, 0, 1, 1, 1, 1, 2, 3, 3 };
+
+        var solution = new _0080_RemoveDuplicatesFromSortedArrayII();
+        var length   = solution.RemoveDuplicates(nums);
+
+        var results = new int[length];
+        for (var i = 0; i < results.Length; i++)
+        {
+            results[i] = nums[i];
+        }
+
+        Assert.Equal(results, new[] { 0, 0, 1, 1, 2, 3, 3 });
+    }
+
+    [Fact]
+    public void TestSingle()
+    {
+        var nums = new[] { 0 };
+
+        var solution = new _0080_RemoveDuplicatesFromSortedArrayII();
+        var length   = solution.RemoveDuplicates(nums);
+
+        var results = new int[length];
+        for (var i = 0; i < results.Length; i++)
+        {
+            results[i] = nums[i];
+        }
+
+        Assert.Equal(results, new[] { 0 });
+    }
+
+    [Fact]
+    public void TestTwoSame()
+    {
+        var nums = new[] { 0, 0 };
+
+        var solution = new _0080_RemoveDuplicatesFromSortedArrayII();
+        var length   = solution.RemoveDuplicates(nums);
+
+        var results = new int[length];
+        for (var i = 0; i < results.Length; i++)
+        {
+            results[i] = nums[i];
+        }
+
+        Assert.Equal(results, new[] { 0, 0 });
+    }
+
+    [Fact]
+    public void TestAllSame()
+    {
+        var nums = new[] { 0, 0, 0, 0, 0, 0 };
+
+        var solution = new _0080_RemoveDuplicatesFromSortedArrayII();
+        var length   = solution.RemoveDuplicates(nums);
+
+        var results = new int[length];
+        for (var i = 0; i < results.Length; i++)
+        {
+            results[i] = nums[i];
+        }
+
+        Assert.Equal(results, new[] { 0, 0 });
+    }
+
+    [Fact]
+    public void TestTwoDiff()
+    {
+        var nums = new[] { 0, 1 };
+
+        var solution = new _0080_RemoveDuplicatesFromSortedArrayII();
+        var length   = solution.RemoveDuplicates(nums);
+
+        var results = new int[length];
+        for (var i = 0; i < results.Length; i++)
+        {
+            results[i] = nums[i];
+        }
+
+        Assert.Equal(results, new[] { 0, 1 });
+    }
+
+    [Fact]
+    public void TestTwoGroupDiff()
+    {
+        var nums = new[] { 0, 0, 1, 1 };
+
+        var solution = new _0080_RemoveDuplicatesFromSortedArrayII();
+        var length   = solution.RemoveDuplicates(nums);
+
+        var results = new int[length];
+        for (var i = 0; i < results.Length; i++)
+        {
+            results[i] = nums[i];
+        }
+
+        Assert.Equal(results, new[] { 0, 0, 1, 1 });
+    }
+
+    [Fact]
+    public void TestAllLongerThanOne()
+    {
+        var nums = new[] { 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 3, 4, 4 };
+
+        var solution = new _0080_RemoveDuplicatesFromSortedArrayII();
+        var length   = solution.RemoveDuplicates(nums);
+
+        var results = new int[length];
+        for (var i = 0; i < results.Length; i++)
+        {
+            results[i] = nums[i];
+        }
+
+        Assert.Equal(results, new[] { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4 });
+    }
+
+    [Fact]
+    public void TestAllLongerThanTwo()
+    {
+        var nums = new[] { 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4 };
+
+        var solution = new _0080_RemoveDuplicatesFromSortedArrayII();
+        var length   = solution.RemoveDuplicates(nums);
+
+        var results = new int[length];
+        for (var i = 0; i < results.Length; i++)
+        {
+            results[i] = nums[i];
+        }
+
+        Assert.Equal(results, new[] { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4 });
+    }
+}
---
+++ LeetCode/_0080_RemoveDuplicatesFromSortedArrayII.cs
@@ -0,0 +1,66 @@
+﻿namespace LeetCode;
+
+public class _0080_RemoveDuplicatesFromSortedArrayII
+{
+    public int RemoveDuplicates_Reference(int[] nums)
+    {
+        if (nums.Length <= 2)
+        {
+            return nums.Length;
+        }
+
+        var slow = 2;
+        var fast = 2;
+
+        while (fast < nums.Length)
+        {
+            if (nums[slow - 2] != nums[fast])
+            {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+
+            fast++;
+        }
+
+        return slow;
+    }
+
+    public int RemoveDuplicates(int[] nums)
+    {
+        var duplicate = nums[0];
+
+        var slow = 0;
+        var fast = 1;
+
+        var isDuplicated = false;
+
+        while (fast < nums.Length)
+        {
+            if (nums[fast] == duplicate)
+            {
+                if (!isDuplicated)
+                {
+                    isDuplicated   = true;
+                    nums[slow + 1] = duplicate;
+                    slow++;
+                }
+            }
+            else
+            {
+                if (isDuplicated)
+                {
+                    isDuplicated = false;
+                }
+
+                nums[slow + 1] = nums[fast];
+                duplicate      = nums[fast];
+                slow++;
+            }
+
+            fast++;
+        }
+
+        return slow + 1;
+    }
+}
