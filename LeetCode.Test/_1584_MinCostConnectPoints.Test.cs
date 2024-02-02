e0d5005f1d153d05d185e7ae1435dedce1b7f36b
imckl <imckl@outlook.com>
2024-02-02 19:45:42 +0800

    update: _1584_MinCostConnectPoints

---
+++ LeetCode.Test/_1584_MinCostConnectPoints.Test.cs
@@ -0,0 +1,157 @@
+﻿namespace LeetCode.Test;
+
+public class _1584_MinCostConnectPoints_Test
+{
+    // [Fact]
+    // public void Test_Normal1()
+    // {
+    //     var points = new[]
+    //     {
+    //         new[] { 0, 0 },
+    //         new[] { 2, 2 },
+    //         new[] { 3, 10 },
+    //         new[] { 5, 2 },
+    //         new[] { 7, 0 }
+    //     };
+    //
+    //     var solution = new _1584_MinCostConnectPoints();
+    //     var actual   = solution.MinCostConnectPoints(points);
+    //
+    //     Assert.Equal(20, actual);
+    // }
+    //
+    // [Fact]
+    // public void Test_Normal2()
+    // {
+    //     var points = new[]
+    //     {
+    //         new[] { 3, 12 },
+    //         new[] { -2, 5 },
+    //         new[] { -4, 1 }
+    //     };
+    //
+    //     var solution = new _1584_MinCostConnectPoints();
+    //     var actual   = solution.MinCostConnectPoints(points);
+    //
+    //     Assert.Equal(18, actual);
+    // }
+    //
+    // [Fact]
+    // public void Test_Normal3()
+    // {
+    //     var points = new[]
+    //     {
+    //         new[] { -1000000, -1000000 },
+    //         new[] { 1000000, 1000000 }
+    //     };
+    //
+    //     var solution = new _1584_MinCostConnectPoints();
+    //     var actual   = solution.MinCostConnectPoints(points);
+    //
+    //     Assert.Equal(4000000, actual);
+    // }
+    //
+    // [Fact]
+    // public void Test_Normal4()
+    // {
+    //     var points = new[]
+    //     {
+    //         new[] { 0, 0 },
+    //         new[] { 2, 2 },
+    //         new[] { 3, 10 },
+    //         new[] { 5, 2 },
+    //         new[] { 7, 0 },
+    //         new[] { 3, 12 },
+    //         new[] { -2, 5 },
+    //         new[] { -4, 1 },
+    //         new[] { -1000000, -1000000 },
+    //         new[] { 1000000, 1000000 }
+    //     };
+    //
+    //     var solution = new _1584_MinCostConnectPoints();
+    //     var actual   = solution.MinCostConnectPoints(points);
+    //
+    //     Assert.Equal(4000015, actual);
+    // }
+    //
+    // [Fact]
+    // public void Test_Normal5()
+    // {
+    //     var points = new[]
+    //     {
+    //         new[] { 0, 0 },
+    //         new[] { 1, 1 },
+    //         new[] { 1, 0 },
+    //         new[] { -1, 1 }
+    //     };
+    //
+    //     var solution = new _1584_MinCostConnectPoints();
+    //     var actual   = solution.MinCostConnectPoints(points);
+    //
+    //     Assert.Equal(4, actual);
+    // }
+    //
+    // [Fact]
+    // public void Test_Normal6()
+    // {
+    //     var points = new[]
+    //     {
+    //         // [-8,14],[16,-18],[-19,-13],[-18,19],[20,20],[13,-20],[-15,9],[-4,-8]
+    //         new[] { -8,14 },
+    //         new[] { 16,-18 },
+    //         new[] { -19,-13 },
+    //         new[] { -18,19},
+    //         new[] {20,20},
+    //         new[] {13,-20},
+    //         new[] {-15,9},
+    //         new[] {-4,-8},
+    //     };
+    //
+    //     var solution = new _1584_MinCostConnectPoints();
+    //     var actual   = solution.MinCostConnectPoints(points);
+    //
+    //     Assert.Equal(139, actual);
+    // }
+    //
+    // [Fact]
+    // public void Test_Normal7()
+    // {
+    //     var points = new[]
+    //     {
+    //         new[] { 7, 18 },
+    //         new[] { -15, 19 },
+    //         new[] { -18, -15 },
+    //         new[] { -7, 14 },
+    //         new[] { 4, 1 },
+    //         new[] { -6, 3 },
+    //     };
+    //
+    //     var solution = new _1584_MinCostConnectPoints();
+    //     var actual   = solution.MinCostConnectPoints(points);
+    //
+    //     Assert.Equal(85, actual);
+    // }
+    //
+    // [Fact]
+    // public void Test_Normal8()
+    // {
+    //     var points = new[]
+    //     {
+    //         new[] { 11,-6 },
+    //         new[] { 9,-19 },
+    //         new[] { 16,-13 },
+    //         new[] { 4,-9},
+    //         new[] {20,4 },
+    //         new[] { 20,7 },
+    //         new[]{-9,18},
+    //         new[]{10,-15},
+    //         new[]{-15,3},
+    //         new[]{6,6}
+    //     };
+    //
+    //     var solution = new _1584_MinCostConnectPoints();
+    //     var actual   = solution.MinCostConnectPoints(points);
+    //
+    //     Assert.Equal(113, actual);
+    // }
+}
---
+++ LeetCode/_1584_MinCostConnectPoints.cs
@@ -0,0 +1,83 @@
+﻿namespace LeetCode;
+
+public class _1584_MinCostConnectPoints
+{
+    // readonly record struct PointWithDistance(int Distance, int X, int Y);
+    //
+    // // public int MinCostConnectPoints(int[][] points)
+    // // {
+    // //     // var pointsWithDistance = new List<(int distance, int[] point)>();
+    // //     //
+    // //     // var n = points.Length;
+    // //     //
+    // //     // for (var i = 0; i < n; i++)
+    // //     // {
+    // //     //     for (var j = i + 1; j < n; j++)
+    // //     //     {
+    // //     //         var pointWithDistance = new PointWithDistance(
+    // //     //             CalcDistance(points[i], points[j]), points[j][0], points[j][1]);
+    // //     //     }
+    // //     // }
+    // //
+    // //     // if (points.Length == 1)
+    // //     // {
+    // //     //     return 0;
+    // //     // }
+    // //     //
+    // //     // var ans = 0;
+    // //
+    // //     // var farthestPoint = points.Select(point =>
+    // //     //     {
+    // //     //         var distance = CalcDistance(point, new[] { 0, 0 });
+    // //     //         return (distance, point);
+    // //     //     })
+    // //     //     .MaxBy(x => x.distance)
+    // //     //     .point ?? throw new NullReferenceException();
+    // //     //
+    // //     // points = points.Select(point =>
+    // //     //     {
+    // //     //         var distance = CalcDistance(point, farthestPoint);
+    // //     //         return (distance, point);
+    // //     //     })
+    // //     //     .OrderBy(x => x.distance)
+    // //     //     .Select(x => x.point)
+    // //     //     .ToArray();
+    // //
+    // //     // points = points.Select(point =>
+    // //     //     {
+    // //     //         var distance = CalcDistance(point, new[] { 0, 0 });
+    // //     //         return (distance, point);
+    // //     //     })
+    // //     //     .OrderBy(x => x.distance)
+    // //     //     .Select(x => x.point)
+    // //     //     .ToArray();
+    // //
+    // //     // Array.Sort(points, (a, b) => a[0] < b[0] ? -1 : a[0] == b[0] ? 0 : 1);
+    // //     // var newPoints = points.OrderBy(x => x[0]).ThenBy(x => x[1]).ToList();
+    // //
+    // //     for (var i = 1; i < points.Length; i++)
+    // //     {
+    // //         var distance = int.MaxValue;
+    // //
+    // //         for (var j = 0; j < i; j++)
+    // //         {
+    // //             var curr = CalcDistance(points[i], points[j]);
+    // //             distance = Math.Min(curr, distance);
+    // //         }
+    // //
+    // //         ans += distance;
+    // //     }
+    // //
+    // //     return ans;
+    // // }
+    //
+    // public int MinCostConnectPoints(int[][] points)
+    // {
+    //     var ans = 0;
+    //
+    //     var n = points.Length;
+    // }
+    //
+    // int CalcDistance(int[] pointA, int[] pointB) =>
+    //     Math.Abs(pointA[0] - pointB[0]) + Math.Abs(pointA[1] - pointB[1]);
+}
