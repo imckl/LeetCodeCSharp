e402699fbb7da7d1da8ad4673c083ec2ad13759a
imckl <imckl@outlook.com>
2024-02-02 22:27:19 +0800

    update: _0027_RemoveElement

---
+++ LeetCode.Test/_0027_RemoveElement.Test.cs
@@ -0,0 +1,184 @@
+﻿namespace LeetCode.Test;
+
+public class _0027_RemoveElement_Test
+{
+    [Fact]
+    public void Test1()
+    {
+        var nums = new[] { 3, 2, 2, 3 };
+        var val  = 3;
+
+        var solution = new _0027_RemoveElement();
+        var length   = solution.RemoveElement(nums, val);
+
+        var results = new int[length];
+        for (var i = 0; i < results.Length; i++)
+        {
+            results[i] = nums[i];
+        }
+
+        Assert.Equivalent(results, new[] { 2, 2 });
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var nums = new[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+        var val  = 2;
+
+        var solution = new _0027_RemoveElement();
+        var length   = solution.RemoveElement(nums, val);
+
+        var results = new int[length];
+        for (var i = 0; i < results.Length; i++)
+        {
+            results[i] = nums[i];
+        }
+
+        Assert.Equivalent(results, new[] { 0, 1, 3, 0, 4 });
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var nums = new[] { 0, 0, 1, 0, 0 };
+        var val  = 0;
+
+        var solution = new _0027_RemoveElement();
+        var length   = solution.RemoveElement(nums, val);
+
+        var results = new int[length];
+        for (var i = 0; i < results.Length; i++)
+        {
+            results[i] = nums[i];
+        }
+
+        Assert.Equivalent(results, new[] { 1 });
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        var nums = new[] { 1, 0, 0, 0, 0 };
+        var val  = 0;
+
+        var solution = new _0027_RemoveElement();
+        var length   = solution.RemoveElement(nums, val);
+
+        var results = new int[length];
+        for (var i = 0; i < results.Length; i++)
+        {
+            results[i] = nums[i];
+        }
+
+        Assert.Equivalent(results, new[] { 1 });
+    }
+
+    [Fact]
+    public void Test5()
+    {
+        var nums = new[] { 0, 0, 0, 0, 1 };
+        var val  = 0;
+
+        var solution = new _0027_RemoveElement();
+        var length   = solution.RemoveElement(nums, val);
+
+        var results = new int[length];
+        for (var i = 0; i < results.Length; i++)
+        {
+            results[i] = nums[i];
+        }
+
+        Assert.Equivalent(results, new[] { 1 });
+    }
+
+    [Fact]
+    public void TestAllIsVal()
+    {
+        var nums = new[] { 0, 0, 0, 0, 0 };
+        var val  = 0;
+
+        var solution = new _0027_RemoveElement();
+        var length   = solution.RemoveElement(nums, val);
+
+        var results = new int[length];
+        for (var i = 0; i < results.Length; i++)
+        {
+            results[i] = nums[i];
+        }
+
+        Assert.Equivalent(results, Array.Empty<int>());
+    }
+
+    [Fact]
+    public void TestAllIsNotVal()
+    {
+        var nums = new[] { 1, 1, 1, 1, 1 };
+        var val  = 0;
+
+        var solution = new _0027_RemoveElement();
+        var length   = solution.RemoveElement(nums, val);
+
+        var results = new int[length];
+        for (var i = 0; i < results.Length; i++)
+        {
+            results[i] = nums[i];
+        }
+
+        Assert.Equivalent(results, new[] { 1, 1, 1, 1, 1 });
+    }
+
+    [Fact]
+    public void TestSingleIsNotVal()
+    {
+        var nums = new[] { 2 };
+        var val  = 3;
+
+        var solution = new _0027_RemoveElement();
+        var length   = solution.RemoveElement(nums, val);
+
+        var results = new int[length];
+        for (var i = 0; i < results.Length; i++)
+        {
+            results[i] = nums[i];
+        }
+
+        Assert.Equivalent(results, new[] { 2 });
+    }
+
+    [Fact]
+    public void TestSingleIsVal()
+    {
+        var nums = new[] { 2 };
+        var val  = 2;
+
+        var solution = new _0027_RemoveElement();
+        var length   = solution.RemoveElement(nums, val);
+
+        var results = new int[length];
+        for (var i = 0; i < results.Length; i++)
+        {
+            results[i] = nums[i];
+        }
+
+        Assert.Equivalent(results, Array.Empty<int>());
+    }
+    
+    [Fact]
+    public void TestEmpty()
+    {
+        var nums = Array.Empty<int>();
+        var val  = 2;
+
+        var solution = new _0027_RemoveElement();
+        var length   = solution.RemoveElement(nums, val);
+
+        var results = new int[length];
+        for (var i = 0; i < results.Length; i++)
+        {
+            results[i] = nums[i];
+        }
+
+        Assert.Equivalent(results, Array.Empty<int>());
+    }
+}
---
+++ LeetCode/_0027_RemoveElement.cs
@@ -0,0 +1,102 @@
+﻿namespace LeetCode;
+
+public class _0027_RemoveElement
+{
+    public int RemoveElement_Optimize(int[] nums, int val)
+    {
+        var left  = 0;
+        var right = nums.Length;
+
+        while (left < right)
+        {
+            if (nums[left] == val)
+            {
+                nums[left] = nums[right - 1];
+                right--;
+            }
+            else
+            {
+                left++;
+            }
+        }
+
+        return left;
+    }
+
+    public int RemoveElement(int[] nums, int val)
+    {
+        var left  = 0;
+        var right = nums.Length - 1;
+
+        while (left < right)
+        {
+            if (nums[left] == val)
+            {
+                nums[left]  = nums[right];
+                nums[right] = val;
+
+                right--;
+
+                if (nums[left] == val)
+                {
+                    continue;
+                }
+            }
+
+            left++;
+        }
+
+        var length = 0;
+
+        foreach (var num in nums)
+        {
+            if (num != val)
+            {
+                length++;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return length;
+    }
+
+    // public int RemoveElement(int[] nums, int val)
+    // {
+    //     var last = nums.Length;
+    //
+    //     var length = 0;
+    //
+    //     var i = 0;
+    //     
+    //     while (i < last)
+    //     {
+    //         if (nums[i] == val)
+    //         {
+    //             while (last > i && nums[last - 1] == val)
+    //             {
+    //                 last--;
+    //             }
+    //             
+    //             if (last == i)
+    //             {
+    //                 return i;
+    //             }
+    //
+    //             nums[i] = nums[last - 1];
+    //
+    //             nums[last - 1] = val;
+    //
+    //             last--;
+    //
+    //             length++;
+    //         }
+    //
+    //         i++;
+    //     }
+    //
+    //     return length;
+    // }
+}
