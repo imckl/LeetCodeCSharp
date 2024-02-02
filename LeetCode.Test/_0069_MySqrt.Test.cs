b3feffce05766eb07f4d258ee456fe7c2ad283bb
imckl <imckl@outlook.com>
2024-02-02 19:45:27 +0800

    update: _0069_MySqrt_Test

---
+++ LeetCode.Test/_0069_MySqrt.Test.cs
@@ -0,0 +1,81 @@
+﻿namespace LeetCode.Test;
+
+public class _0069_MySqrt_Test
+{
+    [Fact]
+    public void Test_1()
+    {
+        var x = 1;
+
+        var solution = new _0069_MySqrt();
+        var actual   = solution.MySqrt(x);
+
+        Assert.Equal(1, actual);
+    }
+    
+    [Fact]
+    public void Test_2()
+    {
+        var x = 2;
+
+        var solution = new _0069_MySqrt();
+        var actual   = solution.MySqrt(x);
+
+        Assert.Equal(1, actual);
+    }
+    
+    [Fact]
+    public void Test_100()
+    {
+        var x = 100;
+
+        var solution = new _0069_MySqrt();
+        var actual   = solution.MySqrt(x);
+
+        Assert.Equal(10, actual);
+    }
+
+    [Fact]
+    public void Test_101()
+    {
+        var x = 101;
+
+        var solution = new _0069_MySqrt();
+        var actual   = solution.MySqrt(x);
+
+        Assert.Equal(10, actual);
+    }
+
+    [Fact]
+    public void Test_99()
+    {
+        var x = 99;
+
+        var solution = new _0069_MySqrt();
+        var actual   = solution.MySqrt(x);
+
+        Assert.Equal(9, actual);
+    }
+
+    [Fact]
+    public void Test_LeftBoundary()
+    {
+        var x = 0;
+
+        var solution = new _0069_MySqrt();
+        var actual   = solution.MySqrt(x);
+
+        Assert.Equal(0, actual);
+    }
+
+    [Fact]
+    public void Test_RightBoundary()
+    {
+        var x = int.MaxValue;
+
+        var solution = new _0069_MySqrt();
+        var actual   = solution.MySqrt(x);
+
+        Assert.Equal(46340, actual);
+    }
+}
---
+++ LeetCode/_0069_MySqrt.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode;
+
+public class _0069_MySqrt
+{
+    public int MySqrt(int x)
+    {
+        if (x == 1)
+        {
+            return 1;
+        }
+
+        var left  = 0;
+        var right = x;
+
+        while (left < right - 1)
+        {
+            var mid = (right + left) >> 1;
+
+            var powMid = (long)mid * mid;
+
+            if (x < powMid)
+            {
+                right = mid;
+            }
+            else if (x > powMid)
+            {
+                left = mid;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+
+        return left;
+    }
+}
